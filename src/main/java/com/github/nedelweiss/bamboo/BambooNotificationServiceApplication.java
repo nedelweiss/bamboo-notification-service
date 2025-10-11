a94c61473c00f951c91112c109aca3520be71dcd
nedelweiss <edelweissnull@gmail.com>
2025-10-12 02:20:36 +0300

    Initial commit

---
+++ src/main/java/com/github/nedelweiss/bamboo/BambooNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.nedelweiss.bamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nedelweiss/bamboo/BambooNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nedelweiss.bamboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
